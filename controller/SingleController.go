058babb8b4ba54338cd595ab234b0267fff8f801
lj99boy <lj99boy@hotmail.com>
2019-11-11 23:57:15 +0800

    dev init

--- controller/SingleController.go
+++ controller/SingleController.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"restful-redis-manager/repo"
 )
@@ -10,18 +11,53 @@ import (
 func StringsHandleFunc(w http.ResponseWriter, r *http.Request) {
 	method := r.Method
 
-	reqSource := r.URL.Query()["source"][0]
-	key := r.URL.Query()["key"][0]
+	reqSource := r.URL.Query().Get("source")
+	key := r.URL.Query().Get("key")
+	val := r.URL.Query().Get("val")
 
 	switch method {
 	case "GET":
 		if reqSource == "" || key == "" {
+			w.WriteHeader(http.StatusBadRequest)
 			fmt.Fprintf(w, "nil source")
-		} else {
-			inputSource := &repo.InputSource{}
-			json.Unmarshal([]byte(reqSource), inputSource)
-			val := repo.GetStringByKey(key, inputSource)
-			fmt.Fprintf(w, val)
+			return
 		}
+
+		inputSource, err := convertJsonStrToSource(reqSource)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "invalid request")
+			return
+		}
+
+		val := repo.GetStringByKey(key, inputSource)
+		fmt.Fprintf(w, val)
+	case "PUT":
+		if reqSource == "" || key == "" || val == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "invalid request")
+			return
+		}
+
+		inputSource, err := convertJsonStrToSource(reqSource)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "invalid request")
+			return
+		}
+
+		repo.SetStrings(inputSource, key, val)
+	}
+}
+
+func convertJsonStrToSource(reqSource string) (*repo.InputSource, error) {
+	inputSource := &repo.InputSource{}
+	err := json.Unmarshal([]byte(reqSource), inputSource)
+	if err != nil {
+		return nil, err
+	} else {
+		return inputSource, nil
 	}
 }
--- repo/SingleRedisRepo.go
+++ repo/SingleRedisRepo.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"github.com/go-redis/redis/v7"
+	"log"
 	"restful-redis-manager/model"
 )
 
@@ -13,7 +14,20 @@ type InputSource struct {
 
 func GetStringByKey(key string, options *InputSource) string {
 	sr := fetchClient(options)
-	return sr.Client.Get(key).String()
+	return sr.Client.Get(key).Val()
+}
+
+func SetStrings(options *InputSource, key string, val string) bool {
+	sr := fetchClient(options)
+
+	res := sr.Client.Set(key, val, 0)
+	err := res.Err()
+	if err != nil {
+		log.Println(err)
+		return false
+	} else {
+		return true
+	}
 }
 
 func fetchClient(options *InputSource) *model.SingleResource {
